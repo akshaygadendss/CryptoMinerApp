27f124666a6ca4bac582c0eba7d7ad4b02ce81c6
Akshay <akshay.gade.ndss@gmail.com>
2025-11-17 14:26:06 +0530

    apk tested successsfully now move to admin dashboard

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,8 +1,8 @@
 import axios from 'axios';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
-const API_URL = 'http://192.168.1.42:3000/api';
-//const API_URL = 'http://10.97.121.196:3000/api';
+//const API_URL = 'http://192.168.1.42:3000/api';
+const API_URL = 'https://cryptominerapp.onrender.com/api';
 
 export interface User {
   _id: string;
